d983f7d285b0519657af9d8f800e1e3aa8efe80f
Dc-Valerian <valerianpedro03@gmail.com>
2025-07-06 15:58:47 +0100

    added password generator

---
+++ cos.py
@@ -0,0 +1,38 @@
+# name = input ("enter your name")
+# print=("Hello",name)
+
+
+# score= int(input ("enter score:"))
+# if score > = 50;
+#     print("pass")
+# else:print("fail")
+
+
+# A = int(input("Enter A:"))
+# B = int(input("Enter B:"))
+
+# sum = A + B
+# print("sum is", sum)
+
+
+# EXAMPLE  1
+# N = int (input("Enter value of N:"))
+# sum = 0 
+
+# for i in range(1 ,N+1):
+#     sum =+ i
+# print("Sum of N Natural No",+ sum)
+
+# EXAMPLE 2
+A = int (input("Enter A:"))
+B = int (input("Enter B:"))
+C = int (input("Enter c:"))
+
+
+if A > B  and A > C:
+  max_val =A 
+elif B > C :
+    max_val = B
+else:
+   max_val = C
+print("Maximum Value is:", max_val)
